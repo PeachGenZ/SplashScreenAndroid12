39bb51199a5990314a685ba3894ac30a7b8f9cf3
Srithep Witayapanpracha <srithep555@gmail.com>
2022-03-13 19:02:46 +0700

    Add splash screen.

--- app/src/main/java/com/peachgenz/splashscreenandroid12/MainActivity.kt
+++ app/src/main/java/com/peachgenz/splashscreenandroid12/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.peachgenz.splashscreenandroid12
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value == true
+            }
+        }
         setContentView(R.layout.activity_main)
     }
 }
